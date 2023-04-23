ee4d3281ec923ee234e4f0941a9375577da77eae
Nicholas <charnick60@gmail.com>
2023-04-23 19:52:35 -0400

    commit 2

--- tracing.js
+++ tracing.js
@@ -1,3 +1,4 @@
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
 const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
@@ -9,9 +10,19 @@ const { ExpressInstrumentation } = require("opentelemetry-instrumentation-expres
 const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+const options = {
+    tags: [], // optional
+    // You can use the default UDPSender
+    host: 'localhost', // optional
+    port: 6832, // optional
+    // OR you can use the HTTPSender as follows
+    // endpoint: 'http://localhost:14268/api/traces',
+    maxPacketSize: 65000 // optional
+  }
+
 //Exporter
 module.exports = (serviceName) => {
-   const exporter = new ConsoleSpanExporter();
+    const exporter = new JaegerExporter(options);
    const provider = new NodeTracerProvider({
        resource: new Resource({
            [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
